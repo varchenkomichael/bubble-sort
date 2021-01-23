9a03d823ddb9d49e4818de0bd3b67c945f53cec2
michael varchenko <Mishanjav2001@gmail.com>
2021-01-23 20:14:04 +0200

    добавил различия

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
                     }
                 }
             }
-            System.out.println(Arrays.toString(arr));
+                 System.out.println(Arrays.toString(arr));
         }
         }
 
